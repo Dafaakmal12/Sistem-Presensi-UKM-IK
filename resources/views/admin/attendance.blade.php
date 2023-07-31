1ad23c217d5d25ef5f6a2ad6b726aec5a8cea6fe
Dafaakmal12 <dafaakmal01@gmail.com>
2023-07-31 17:45:28 +0700

    sistem-presensi-ik

--- resources/views/admin/attendance.blade.php
+++ resources/views/admin/attendance.blade.php
@@ -1,115 +1,151 @@
 @extends('layouts.admin')
 
 @section('content')
-<form action="{{ route('admin.attendance') }}" method="GET" >
+<head>
+    <!-- ... other meta tags and stylesheets ... -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.2/xlsx.full.min.js"></script>
+</head>
+
+<div class="flex justify-center"><br/>
+<h2 class="text-xl font-semibold text-gray-900 dark:text-white mb-4 ">Daftar Presensi Agenda</h2>
+</div><br/>
+<form action="{{ route('admin.attendance') }}" method="GET">
     @csrf
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                            <th scope="col" class="px-6 py-3">
-                                Nama Agenda
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Tanggal
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Mulai
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Berakhir
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                File
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Action
-                            </th>
-                        </tr>
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Nama Agenda
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Tanggal
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Mulai
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Berakhir
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        File
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
             </thead>
-           <tbody>
-           @if($agenda->count() == 0)
+            <tbody>
+                @if($agenda->count() == 0)
+                <tr>
+                    <td colspan="5" class="text-center">Tidak ada data</td>
+                </tr>
+                @endif
+                @foreach($agenda as $item)
+                <tr>
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $item->nama }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $item->dateTime->format('d-m-Y') }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $item->startTime->format('H:i') }} WIB
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $item->endTime->format('H:i') }} WIB
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="{{ route('agenda.download', $item->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                            Download
+                        </a>
+                    </td>
+                    <td class="px-6 py-4">
+                        <button data-modal-target="defaultModal-{{$item->id}}" data-modal-toggle="defaultModal-{{$item->id}}" class="font-medium text-green-600 dark:text-green-500 hover:underline" type="button">
+                            Detail Presensi
+                        </button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</form>
+
+@foreach($agenda as $item)
+<!-- Main modal -->
+<div id="defaultModal-{{$item->id}}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-2 right-0 z-50 hidden w-full p-4 md:inset-0">
+    <div class="relative w-full h-full max-w-2xl mx-auto bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal content -->
+        <div class="p-4 md:p-6 space-y-6">
+            <div class="flex items-start justify-between pb-4 border-b">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Deskripsi Agenda
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 md:p-2 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal-{{$item->id}}">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="max-h-80 md:max-h-full overflow-y-auto">
+                <h3>{!! nl2br(e($item->deskripsi)) !!}</h3>
+            </div>
+            <div class="w-full my-2 text-center">
+                <h4 class="text-2xl font-bold">Daftar Hadir {{ $item->nama }}</h4>
+                <button onclick="exportToExcel('{{ $item->nama }}')" class="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-blue-600 float-right">
+                    Export
+                </button>
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <td colspan="5" class="text-center">Tidak ada data</td>
+                            <th class="px-6 py-3">No</th>
+                            <th class="px-6 py-3">NRA</th>
+                            <th class="px-6 py-3">Nama</th>
+                            <th class="px-6 py-3">Status</th>
                         </tr>
-                        @endif
-                        @foreach($agenda as $item)
-
+                    </thead>
+                    <tbody>
+                        <!-- Replace this with your Blade loop to show the attendance data -->
                         <tr>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{ $item->nama }}
-                            </th>
                             <td class="px-6 py-4">
-                                {{ $item->dateTime->format('d-m-Y') }}
+                                1
                             </td>
                             <td class="px-6 py-4">
-                                {{ $item->startTime->format('H:i') }} WIB
+                                NRA12345
                             </td>
                             <td class="px-6 py-4">
-                                {{ $item->endTime->format('H:i') }} WIB
+                                John Doe
                             </td>
                             <td class="px-6 py-4">
-                                <a href="{{ route('agenda.download', $item->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                                    Download</a>
-                            </td>
-                            </td>
-                            <td class="px-6 py-4">
-                                <button data-modal-target="defaultModal-{{$item->id}}" data-modal-toggle="defaultModal-{{$item->id}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" type="button">
-                                    Show
-                                </button>
-
-                                <!-- <form action="{{ route('agenda.updateagenda', $item->id) }}" method="GET">
-                                    @csrf
-                                    @method('GET')
-                                    <button type="submit" class="font-medium text-green-600 dark:text-green-500 hover:underline">Update</button>
-                                </form>
-
-                                <form action="{{ route('agenda.delete', $item->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="font-medium text-red-600 dark:text-red-500 hover:underline">
-                                        Delete</button>
-                                </form> -->
+                                Hadir
                             </td>
                         </tr>
-
-                        <!-- Main modal -->
-                        <div id="defaultModal-{{$item->id}}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-2 right-0 z-50 hidden w-full p-4 md:inset-0">
-                            <div class="relative w-full h-full max-w-2xl mx-auto bg-white rounded-lg shadow dark:bg-gray-700">
-                                <!-- Modal content -->
-                                <div class="p-4 md:p-6 space-y-6">
-                                    <div class="flex items-start justify-between pb-4 border-b">
-                                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                            Deskripsi Agenda
-                                        </h3>
-                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 md:p-2 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal-{{$item->id}}">
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                            </svg>
-                                            <span class="sr-only">Close modal</span>
-                                        </button>
-                                    </div>
-                                    <!-- Modal body -->
-                                    <div class="max-h-80 md:max-h-full overflow-y-auto">
-                                        <h3>{!! nl2br(e($item->deskripsi)) !!}</h3>
-                                    </div>
-                                    <!-- Button Presensi -->
-                                    <form action="{{ route('users.attendance') }}" method="POST">
-                                        @csrf
-                                        <input type="hidden" name="id_event" value="{{ $item->id }}">
-                                        <input type="hidden" name="tanggal" value="{{ $item->dateTime }}">
-                                        <button type="submit" class="w-full p-3 text-white bg-yellow-600 rounded-md shadow-md hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50">
-                                            Presensi
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-        </tbody>
-    </table>
+                        <!-- You can add more rows as needed -->
+                    </tbody>
+                </table>
+             </div>
+        </div>
+    </div>
 </div>
+@endforeach
+<script>
+    function exportToExcel(sheetName) {
+        var rows = [
+            ['No', 'NRA', 'Nama', 'Status'],
+            [1, 'NRA12345', 'John Doe', 'Hadir'],
+            // Add more rows as needed
+        ];
 
+        var sheet = XLSX.utils.aoa_to_sheet(rows);
+        var workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, sheet, sheetName);
 
+        /* Export the workbook */
+        XLSX.writeFile(workbook, sheetName + '.xlsx');
+    }
+</script>
 @endsection
 
